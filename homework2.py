2072e910a2a2952ec2701d06d54c9a9f5dd830ac
Jetboy13 <fata86@mail.ru>
2024-07-27 20:19:25 +0400

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,4 @@
+Home_work = 12
+Hours_spent = 1.5
+Course_name = 'Python,'
+print('Курс:',Course_name, 'Всего задач:',Home_work,'затрачено часов:',Hours_spent, 'среднее время выполнени', (Hours_spent / Home_work),'часа')
