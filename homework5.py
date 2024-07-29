03c8ebb140f7b8fe68db14882395cbc252001df1
Jetboy13 <fata86@mail.ru>
2024-07-29 20:27:09 +0400

    Add files via upload

---
+++ homework5.py
@@ -0,0 +1,7 @@
+immutable_var = ('bear', 'fox', 'wolf', 2,)
+print(immutable_var)
+mutable_list = (['sun'], 'rain', 'cloud', 5)
+print(type(mutable_list))
+print(mutable_list)
+mutable_list [0][0] = 'weather'
+print(mutable_list)
