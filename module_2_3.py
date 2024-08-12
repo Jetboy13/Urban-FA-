31543535190399d7763ddf14801e6ba37453a349
Jetboy13 <fata86@mail.ru>
2024-08-12 18:13:25 +0400

    Update module_2_3.py

--- module_2_3.py
+++ module_2_3.py
@@ -1,7 +1,10 @@
 my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
 a = 0
-while 0 <= len(my_list):
-    if my_list[a] >= 0:
+while 0 < len(my_list):
+    if my_list[a] > 0:
         print(my_list[a])
         a += 1
+        if my_list[a] == 0:
+            del my_list[a]
         continue
+    break
